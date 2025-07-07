60eebf86e0dcae4cb9245cdf452e9d503cf178e8
Harika <harika.aaslin@gmail.com>
2025-07-07 21:27:47 +0530

    Assignment4

--- java_Assignment4/CustomException.java
+++ java_Assignment4/CustomException.java
@@ -1,22 +1,22 @@
-import java.io.*;
+
 import java.util.Scanner;
 public class CustomException{
         public static void checkDet(String uName) throws LoginFailedException{
              if(uName==null || uName.isEmpty()){
-                 throw new LoginFailedException("Invalid Login Details");
+                 throw new LoginFailedException("Invalid Login details");
             }
             System.out.println("Username is valid,login successfully");
         }
     public static void main(String args[]) {
         Scanner sc =new Scanner(System.in);
         String uName=sc.nextLine();
-        String password=sc.nextLine();
         try{
             checkDet(uName);
         }
         catch(LoginFailedException lfe){
             System.out.println("Exception: "+lfe.getMessage());
         }
+        sc.close();
     }
 
 }
