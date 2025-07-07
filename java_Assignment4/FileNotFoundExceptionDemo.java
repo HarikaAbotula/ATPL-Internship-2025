76cf3a901e87f28da622c3df3735d3b85ef29700
Harika <harika.aaslin@gmail.com>
2025-07-07 20:21:10 +0530

    Assignment4

--- java_Assignment4/FileNotFoundExceptionDemo.java
+++ java_Assignment4/FileNotFoundExceptionDemo.java
@@ -3,11 +3,17 @@ import java.io.FileNotFoundException;
 
 public class FileNotFoundExceptionDemo {
     public static void main(String[] args) {
+        FileReader fr=null;
         try {
-            FileReader fr = new FileReader("password.txt");
+            fr = new FileReader("password.txt");
             System.out.println("File opened successfully.");
         } catch (FileNotFoundException e) {
             System.out.println("Caught FileNotFoundException: " + e.getMessage());
+        }
+        finally{
+            if(fr!=null){
+                fr.close();
+            }
         }
     }
 }
