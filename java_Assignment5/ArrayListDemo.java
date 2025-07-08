04711345ab2e5626fbc98ae1f59090168da10690
Harika <harika.aaslin@gmail.com>
2025-07-08 13:49:34 +0530

    Assignment5

--- java_Assignment5/ArrayListDemo.java
+++ java_Assignment5/ArrayListDemo.java
@@ -1,1 +1,23 @@
-import java.io.*;
+import java.util.*;
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<String> list =new ArrayList<>();
+        list.add("Apple");
+        list.add("Mango");
+        list.add("grapes");
+        list.add("kiwi");
+        list.add("banana");
+        System.out.println("Printing using for loop: ");
+        for(int i=0;i<list.size();i++){
+            System.out.println(list.get(i));
+        }
+        //enhanced loop
+         System.out.println("Printing using for each loop: ");
+        for(String a: list){
+             System.out.println(a);
+        }
+        //removing 3rd element
+        System.out.println("The updated list, after removing 3rd element from the list: "+list.remove(2));
+        
+    }
+}
