fb86e71d8b04bda517b221068bd2cd3361b0d8e1
Harika <harika.aaslin@gmail.com>
2025-07-09 11:44:11 +0530

    Assignment5

--- java_Assignment5/VectorDemo.java
+++ java_Assignment5/VectorDemo.java
@@ -1,19 +1,24 @@
 import java.util.*;
 public class VectorDemo {
     public static void main(String[] args) {
-        Vector<Double> decimalNumbers = new Vector<>();
+        Scanner scanner = new Scanner(System.in);
+        Vector<Double> vector = new Vector<>();
+        System.out.println("Enter the elements into the vector");
+        int size=scanner.nextInt();
 
-        decimalNumbers.add(10.5);
-        decimalNumbers.add(20.75);
-        decimalNumbers.add(30.25);
-        decimalNumbers.add(40.0);
+        for(int index=0;index<size;index++){
+            vector.add(scanner.nextDouble());
+        }
 
-        Enumeration<Double> e = decimalNumbers.elements();
+        Enumeration<Double> enumeration = vector.elements();
         System.out.println("Decimal numbers in Vector:");
-        while (e.hasMoreElements()) {
-            Double value = e.nextElement();
+
+        while (enumeration.hasMoreElements()) {
+            Double value = enumeration.nextElement();
             System.out.println(value);
         }
+        
+        scanner.close();
     }
 }
 
