294709cbeed5620df594670ebadb480855b17765
Harika <harika.aaslin@gmail.com>
2025-07-09 11:34:24 +0530

    Assignment5

--- java_Assignment5/StackDemo.java
+++ java_Assignment5/StackDemo.java
@@ -1,28 +1,30 @@
 import java.util.*;
 public class StackDemo {
     public static void main(String[] args) {
-         Scanner sc=new Scanner(System.in);
+        Scanner scanner=new Scanner(System.in);
         Stack<Integer> stack=new Stack<>();
-        System.out.println("Enter Integer input to push on stack,Type FINISH after entering");
-        while(sc.hasNext()){
-            if(sc.hasNextInt()){
-                int num=sc.nextInt();
-                stack.push(num);
+        System.out.println("Enter Integer elements to push on stack,Type FINISH after entering");
+
+        while(scanner.hasNext()){
+            if(scanner.hasNextInt()){
+                int elements=scanner.nextInt();
+                stack.push(elements);
                // System.out.println(num);
             }
             else{
-                String inp=sc.next();
-                if(inp.equalsIgnoreCase("Finish")){
+                String stringinput=scanner.next();
+                if(stringinput.equalsIgnoreCase("Finish")){
                     break;
                 }
                 System.out.println("Invalid input.please provide Integer input");
             }
         }
+
         System.out.println("ORIGINAL STACK: "+stack);
         stack.pop();
         stack.pop();
         System.out.println("STACK AFTER POPPING ELEMENTS: "+stack);
-        sc.close();
+        scanner.close();
         
     }
 }
