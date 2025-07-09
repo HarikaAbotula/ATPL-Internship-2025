3ecc5b0528b0b46bb902dc5a6bc2b7cd5a703909
Harika <harika.aaslin@gmail.com>
2025-07-09 11:29:22 +0530

    Assignment5

--- java_Assignment5/LinkedListDemo.java
+++ java_Assignment5/LinkedListDemo.java
@@ -1,15 +1,19 @@
 import java.util.*;
 public class LinkedListDemo {
     public static void main(String[] args) {
-        LinkedList<Integer> ll =new LinkedList<>();
-        ll.add(10);
-        ll.add(20);
-        ll.add(30);
-        ll.add(40);
-        ll.add(50);
-        ll.addFirst(5);
-        ll.addLast(60);
-        ll.remove(2);
-        System.out.println("Elements present in linked list: "+ll);
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the size of the linkedlist: ");
+        int size=scanner.nextInt();
+        LinkedList<Integer> linkedlist =new LinkedList<>();
+
+        for(int index=0;index<size;index++){
+            linkedlist.add(scanner.nextInt());
+        }
+        System.out.println("Elements in linkedlist: "+linkedlist);
+        linkedlist.addFirst(5);
+        linkedlist.addLast(60);
+        linkedlist.remove(2);
+        System.out.println("Elements present in linked list after performing actions: "+linkedlist);
+        scanner.close();
     }
 }
