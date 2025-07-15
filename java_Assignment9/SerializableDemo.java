ddd528560d9ad90449b3bf839729ecaf5188cb8d
Harika <harika.aaslin@gmail.com>
2025-07-15 13:52:12 +0530

    Assignment9

--- java_Assignment9/SerializableDemo.java
+++ java_Assignment9/SerializableDemo.java
@@ -2,19 +2,26 @@ import java.io.*;
 
 public class SerializableDemo {
     public static void main(String[] args) {
-        Student s1 = new Student(101, "John");
+        Student studentOne = new Student(101, "John",23,"CSE",9.8);
+        Student studentTwo = new Student(102, "Ron",21,"CSE",8.8);
+        Student studentThree = new Student(103, "Jessy",21,"ECE",7.9);
+        Student studentFour = new Student(104, "Steve",22,"IT",8.7);
+        Student studentFive = new Student(105, "Nancy",21,"CSE",8.2);
 
         try {
-            FileOutputStream fos = new FileOutputStream("Student.txt");
-            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            FileOutputStream fileOutputStream = new FileOutputStream("student.ser");
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
 
-            oos.writeObject(s1);  // Serialize the object
-            oos.close();
-            fos.close();
-
+            objectOutputStream.writeObject(studentOne); 
+            objectOutputStream.writeObject(studentTwo); // Serialize the object
+            objectOutputStream.writeObject(studentThree);
+            objectOutputStream.writeObject(studentFour);
+            objectOutputStream.writeObject(studentFive);
+            objectOutputStream.close();
+            fileOutputStream.close();
             System.out.println("Object has been serialized.");
-
-        } catch (IOException e) {
+        } 
+        catch (IOException e) {
             e.printStackTrace();
         }
     }
