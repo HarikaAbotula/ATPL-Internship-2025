3a519f61a5394ff90fb2f729a5fe210eb6f76537
Harika <harika.aaslin@gmail.com>
2025-07-17 17:14:11 +0530

    Assignment4

--- java_Assignment4/CustomExceptionLoginFailed.java
+++ java_Assignment4/CustomExceptionLoginFailed.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 public class CustomExceptionLoginFailed {
+
     public static void login(String uName,String password) throws LoginFailedException{
+        
         if(!uName.matches("^[a-zA-Z][a-zA-Z0-9_]{4,9}$") || !password.matches("^[A-Z][a-z0-9@#$%^&*!]{7,}$")){
             throw new LoginFailedException("Invalid credentials,please provide valid details");
         }
