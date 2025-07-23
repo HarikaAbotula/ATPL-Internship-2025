465024e948801eb56a0f8dabd970c84821e01247
Harika <harika.aaslin@gmail.com>
2025-07-23 20:55:48 +0530

    Assignment

--- Java8_features/FilterNames.java
+++ Java8_features/FilterNames.java
@@ -1,5 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class FilterNames {
     public static void main(String[] args) {
-        
+    
+        List<String> names = Arrays.asList("Alice", "Bob", "Andrew", "Tom", "Angela", "Steve");
+
+        names.stream()
+            .filter(name -> name.startsWith("A"))
+            .forEach(name -> System.out.print(name + " "));
     }
 }
+
+    
--- Java8_features/SortNumbers.java
+++ Java8_features/SortNumbers.java
@@ -1,12 +1,17 @@
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class SortNumbers {
     public static void main(String[] args) {
         List<Integer> numberList = Arrays.asList(5,2,8,1,9);
+        System.out.println("Original List: "+numberList);
 
-        Comparator<Integer> comparator = (
-    }
+        // Sorting in descending order using lambda expression
+        Comparator<Integer> comparator = (number2,number1) -> (number1<number2) ? -1 : (number1>number2) ? 1 : 0;
+         
+        Collections.sort(numberList,comparator); 
+        System.out.println("Descending order List:" + numberList);  
+     }
 }
