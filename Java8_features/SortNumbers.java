382ab723f39ac76fdfa9a2515d8437eb8589d256
Harika <harika.aaslin@gmail.com>
2025-07-23 17:37:20 +0530

    Assignment

---
+++ Java8_features/SortNumbers.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortNumbers {
+    public static void main(String[] args) {
+        List<Integer> numberList = Arrays.asList(5,2,8,1,9);
+
+        Comparator<Integer> comparator = (
+    }
+}
