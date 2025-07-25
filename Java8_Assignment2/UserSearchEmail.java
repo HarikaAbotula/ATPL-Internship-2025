c3922e04970f8397dcb3b94a410bf3d2bbaa12d8
Harika <harika.aaslin@gmail.com>
2025-07-25 10:43:19 +0530

    Assignment

---
+++ Java8_Assignment2/UserSearchEmail.java
@@ -0,0 +1,44 @@
+
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.*;
+
+class User{
+    String name;
+    String email;
+
+    User(String name,String email){
+        this.name = name;
+        this.email = email;
+    }
+    
+    public Optional<String> getEmailOptional() {
+        return Optional.ofNullable(email);
+    }
+}
+public class UserSearchEmail {
+    public static void main(String[] args) {
+        ArrayList<User> usersList = new ArrayList<>();
+            usersList.add(new User("Harika","harika@gmail.com"));
+            usersList.add(new User("Kitty", null));
+            usersList.add(new User("Roshie","rioshini@company.com"));
+            usersList.add(new User("Himaja","himaja@outlook.com"));
+
+        String targetName = "Kitty";  
+
+        Function<String, String> toUpperCaseFunction = String::toUpperCase;
+
+          // 1. Find the user by name using Stream and filter
+        Optional<User> foundUser = usersList.stream()
+                .filter(user -> user.name.equalsIgnoreCase(targetName))
+                .findFirst();
+                
+        String result = foundUser.flatMap(User::getEmailOptional) 
+                .map(toUpperCaseFunction)
+                .orElse("Email not Found");
+                
+        System.out.println(result);
+                
+
+    }
+}
