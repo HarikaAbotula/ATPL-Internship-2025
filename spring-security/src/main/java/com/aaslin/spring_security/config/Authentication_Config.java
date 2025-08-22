af8c6f98072aac3e41c0745a769ab3eae7c092e4
Harika <107036573+HarikaAbotula@users.noreply.github.com>
2025-08-22 23:50:19 +0530

    Update Authentication_Config.java

--- spring-security/src/main/java/com/aaslin/spring_security/config/Authentication_Config.java
+++ spring-security/src/main/java/com/aaslin/spring_security/config/Authentication_Config.java
@@ -14,16 +14,16 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class Authentication_Config {
 	
-//	@Bean
-//	public InMemoryUserDetailsManager userDetailsService() {
-//		UserDetails user = User.withUsername("user").password("{noop}pass123").roles("USER").build();
-//		return new InMemoryUserDetailsManager(user);
-//	}
-//	
-//	@Bean
-//	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//		http.authorizeHttpRequests(auth -> auth.requestMatchers("/api/dev").permitAll().anyRequest().authenticated());
-//		http.formLogin(Customizer.withDefaults());
-//		return http.build();
-//	}
+	@Bean
+	public InMemoryUserDetailsManager userDetailsService() {
+		UserDetails user = User.withUsername("user").password("{noop}pass123").roles("USER").build();
+		return new InMemoryUserDetailsManager(user);
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(auth -> auth.requestMatchers("/api/dev").permitAll().anyRequest().authenticated());
+		http.formLogin(Customizer.withDefaults());
+		return http.build();
+	}
 }
